9ba102d3bd5ba4003d2faa643a5ef6c908be16e7
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-07 13:53:42 -0400

    People created, initialised and tested

--- people.rb
+++ people.rb
@@ -10,14 +10,34 @@ class Person
 
 class Student < Person
 
+  # def initialize(first_name, last_name)
+  #   super(first_name, last_name)
+  # end
+
   def learn
     puts "I get it!"
   end
 end
 
 class Instructor < Person
+
+  # def initialize(first_name, last_name)
+  #   super(first_name, last_name)
+  # end
+
   def teach
     puts "Everything in Ruby is an Object"
   end
 
 end
+
+i = Instructor.new("Chris", "Instructor")
+puts i.full_name
+puts i.teach
+
+s = Student.new("Christina", "Student")
+puts s.full_name
+puts s.learn
+end
+
+# Student isn't a parent class of Instructor and vice versa.
